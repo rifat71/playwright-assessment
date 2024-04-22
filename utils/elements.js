4fb6e0fb5d09aab9e9e279d0b8be62a85d2050c8
rifat71 <rifatmahmud24311@gmail.com>
2024-04-22 13:46:11 +0600

    comment add

--- utils/elements.js
+++ utils/elements.js
@@ -1,5 +1,8 @@
 const { Promise } = require(`bluebird`);
 
+/**
+* Here we will handle user element interactions
+*/
 module.exports = {
   clickElement: async (world, locator, options = {}) => {
     const index = options.index ? options.index : 1;
